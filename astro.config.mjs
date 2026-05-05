a477967040641d69894fe304d71736aa6b3d1451
cairncms-dev <hello@cairncms.dev>
2026-05-05 08:52:27 -0500

    Add basic SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,41 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	site: 'https://cairncms.dev',
 	integrations: [
+		sitemap(),
 		starlight({
 			title: 'CairnCMS',
 			disable404Route: true,
+			head: [
+				{
+					tag: 'script',
+					attrs: {
+						defer: true,
+						src: 'https://cloud.umami.is/script.js',
+						'data-website-id': 'dcd90c8d-1b3b-4445-bfd5-1d7f0703dc88',
+					},
+				},
+				{
+					tag: 'meta',
+					attrs: { property: 'og:image', content: 'https://cairncms.dev/og.png' },
+				},
+				{
+					tag: 'meta',
+					attrs: { property: 'og:site_name', content: 'CairnCMS' },
+				},
+				{
+					tag: 'meta',
+					attrs: { name: 'twitter:card', content: 'summary_large_image' },
+				},
+				{
+					tag: 'meta',
+					attrs: { name: 'twitter:image', content: 'https://cairncms.dev/og.png' },
+				},
+			],
 			sidebar: [
 				{ label: 'Getting started', autogenerate: { directory: 'docs/getting-started' } },
 				{ label: 'Reference', autogenerate: { directory: 'docs/reference' } },
